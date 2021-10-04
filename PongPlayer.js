c08122209cc8d5de03ab2b790c437d18c37ff134
blackhawk27 <oliver1954@outlook.dk>
2021-10-04 13:00:57 +0200

    Paddel update

--- PongPlayer.js
+++ PongPlayer.js
@@ -17,5 +17,26 @@ class Player{
         ctx.fillStyle = "#FFFFFF";
         ctx.fillRect(this.x, this.y, this.w, this.h);
     }
+    
 }
 
+document.addEventListener('keydown', function(event) {
+   
+    //top
+    if(event.keyCode == 38) {
+        playerR.y -= 5;
+    }
+  
+    //bottom
+    else if(event.keyCode == 40) {
+        playerR.y += 5;
+    }
+    // w
+    else if(event.keyCode == 87) {
+        playerL.y -= 5;
+    }
+    // s
+    else if(event.keyCode == 83) {
+        playerL.y += 5;
+    }
+});
