3e5838f86f02a9abda48707202910f9051379ab0
blackhawk27 <oliver1954@outlook.dk>
2021-10-11 11:14:23 +0200

    Pong player update

--- PongMain.js
+++ PongMain.js
@@ -5,8 +5,8 @@ ctx.fillRect(0,0, canvas.width, canvas.height);
 Ball.Dball.push(new Ball(5,5,100,50,0.25,0.33))
 
 
-let playerL = new Player(5, 50, 5, 40);
-let playerR = new Player(290, 50, 5, 40);
+let playerL = new Player(5, 50, 5, 40, 0);
+let playerR = new Player(290, 50, 5, 40, 0);
 
 class Game {
     static instance = new Game();
@@ -29,6 +29,7 @@ loop() {
             
     if(Ball.Dball.length == 0) {
         Ball.Dball.push(new Ball(5,5,100,50,0.25,0.33))
+        // width, height, xpos, ypos, xspeed, yspeed
     }
     
     Ball.Dball[0].move();
@@ -36,7 +37,11 @@ loop() {
     }
 
 collision() {
-    if((Ball.Dball[0].xpos + Ball.Dball[0].width == playerR.x && Ball.Dball[0].ypos <= playerR.y + playerR.h) && (Ball.Dball[0].ypos + Ball.Dball[0].height >= playerR.y) || (Ball.Dball[0].xpos == playerL.x + playerL.w && Ball.Dball[0].ypos <= playerL.y + playerL.h) && (Ball.Dball[0].ypos + Ball.Dball[0].height >= playerL.y)) {
+    if((Ball.Dball[0].xpos + Ball.Dball[0].width == playerR.x && Ball.Dball[0].ypos 
+    <= playerR.y + playerR.h) && (Ball.Dball[0].ypos + Ball.Dball[0].height 
+    >= playerR.y) || (Ball.Dball[0].xpos == playerL.x + playerL.w && Ball.Dball[0].ypos 
+    <= playerL.y + playerL.h) && (Ball.Dball[0].ypos + Ball.Dball[0].height >= playerL.y)) 
+    {
        Ball.Dball[0].xspeed *= -1;
        }
 }
@@ -46,5 +51,7 @@ collision() {
 function gameloop() {
     Game.instance.loop();
     Game.instance.collision();
+    playerL.y += playerL.ySpeed;
+    playerR.y += playerR.ySpeed;
 }
 setInterval(gameloop,1)
--- PongPlayer.js
+++ PongPlayer.js
@@ -1,11 +1,14 @@
 class Player{
 
-    constructor(x,y,w,h){
+    constructor(x,y,w,h,ySpeed){
 
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.ySpeed = ySpeed;
+        
+
 
     }
 
@@ -16,24 +19,50 @@ class Player{
     }
     
 }
+// Class end
+
 
 document.addEventListener('keydown', function(event) {
    
     //top
     if(event.keyCode == 38) {
-        playerR.y -= 5;
+        playerR.ySpeed = -0.5;
     }
   
     //bottom
-    else if(event.keyCode == 40) {
-        playerR.y += 5;
+    if(event.keyCode == 40) {
+        playerR.ySpeed = 0.5;
     }
     // w
-    else if(event.keyCode == 87) {
-        playerL.y -= 5;
+    if(event.keyCode == 87) {
+        playerL.ySpeed = -0.5;
     }
     // s
-    else if(event.keyCode == 83) {
-        playerL.y += 5;
+    if(event.keyCode == 83) {
+        playerL.ySpeed = 0.5;
+    }
+
+});
+
+document.addEventListener('keyup', function(event) {
+   
+  
+
+    if(event.keyCode == 38) {
+        playerR.ySpeed = 0;
+    }
+  
+    //bottom
+    if(event.keyCode == 40) {
+        playerR.ySpeed = 0;
+    }
+    // w
+    if(event.keyCode == 87) {
+        playerL.ySpeed = 0;
+    }
+    // s
+    if(event.keyCode == 83) {
+        playerL.ySpeed = 0;
     }
 });
+
