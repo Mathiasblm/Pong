3f5613925a82edc9dca6f34b4ad5d2bf4ae82db6
LennysGame <73524572+LennysGame@users.noreply.github.com>
2021-10-15 08:58:23 +0200

    Add files via upload

--- PongMain.js
+++ PongMain.js
@@ -1,9 +1,15 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+
+function draw() {
+    ctx.fillRect(0, 0, width, height)
+    ctx.save();
+    ctx.fillStyle = "#fff";
+}
+
 ctx.fillStyle = "black";
 ctx.fillRect(0,0, canvas.width, canvas.height);
 Ball.Dball.push(new Ball(5,5,100,50,0.25,0.33))
-
 
 let playerL = new Player(5, 50, 5, 40, 0);
 let playerR = new Player(290, 50, 5, 40, 0);
@@ -16,6 +22,8 @@ class Game {
 loop() {
     let canvas = document.getElementById("canvas");
     let context = canvas.getContext("2d");
+
+
     context.fillStyle = "#000000"
     context.fillRect(
         0, 
@@ -34,7 +42,18 @@ loop() {
     
     Ball.Dball[0].move();
     Ball.Dball[0].remove();
-    }
+
+    var w = 4;
+var x = (canvas.width - w)*0.5;
+var y = 0;
+var step = canvas.height/20;
+while (y < canvas.height) {
+    ctx.fillRect(x, y+step*0.25, w, step*0.5); 
+    y += step;
+}
+ctx.restore();
+}
+   
 
 collision() {
     if((Ball.Dball[0].xpos + Ball.Dball[0].width == playerR.x && Ball.Dball[0].ypos 
@@ -51,13 +70,7 @@ collision() {
 function gameloop() {
     Game.instance.loop();
     Game.instance.collision();
-    
-    if(playerL.y + playerL.ySpeed > 0 && playerL.y + playerL.ySpeed < canvas.height-playerL.h) {
-        playerL.y += playerL.ySpeed;
-    }
-    
-    if(playerR.y + playerR.ySpeed > 0 && playerR.y + playerR.ySpeed < canvas.height-playerR.h) {
-        playerR.y += playerR.ySpeed;
-    }
+    playerL.y += playerL.ySpeed;
+    playerR.y += playerR.ySpeed;
 }
 setInterval(gameloop,1)
